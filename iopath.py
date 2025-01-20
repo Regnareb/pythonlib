333a7267e2bb5e41545fe24292532e5814526d3c
Regnareb <kun_fu_man@hotmail.com>
2025-01-20 18:25:39 +0100

    Add utils.py with is_admin(), send_mail() and internet()
    Move send_mail() and internet() from iopath to utils

--- iopath.py
+++ iopath.py
@@ -4,14 +4,9 @@ import sys
 import json
 import errno
 import pickle
-import ctypes
-import socket
-import smtplib
 import logging
 import subprocess
 from contextlib import contextmanager
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from . import common
 logger = logging.getLogger(__name__)
 
@@ -143,39 +138,6 @@ def openfile(path):
     elif os.name == 'posix':
         subprocess.call(('xdg-open', path))
 
-
-def internet(host="8.8.8.8", port=53, timeout=3):
-    """
-    Host: 8.8.8.8 (google-public-dns-a.google.com)
-    OpenPort: 53/tcp
-    Service: domain (DNS/TCP)
-    """
-    try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
-    except socket.error as ex:
-        logger.exception(ex)
-        return False
-
-
-def send_mail(sender, receivers, subject, text='', html=''):
-    msg = MIMEMultipart()
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ', '.join(receivers)
-    text = MIMEText(text, 'plain')
-    html = MIMEText(html, 'html')
-    msg.attach(text)
-    msg.attach(html)
-    try:
-        s = smtplib.SMTP('localhost')
-        s.sendmail(sender, receivers, msg.as_string())
-        s.quit()
-        return True
-    except smtplib.SMTPException:
-        logger.error('Error: unable to send email')
-        return False
 
 
 @contextmanager
